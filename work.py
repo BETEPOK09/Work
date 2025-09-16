19b0e8db7b56d233dadfa2036ea842fa38df6c93
BETEPOK09 <LeoMix09@yandex.ru>
2025-09-16 20:07:50 +0400

    Исправленная версия

--- work.py
+++ work.py
@@ -10,7 +10,8 @@ import os
 MAX_WIDTH = 1200
 SUFFIX = '_process'
 
-#Функция для обработки изображений
+
+# Функция для обработки изображений
 def processed_image(path, out_jpegs_dir, out_webps_dir):
     try:
 
@@ -28,16 +29,19 @@ def processed_image(path, out_jpegs_dir, out_webps_dir):
             if img.mode != "RGBA":
                 img = img.convert('RGBA')
 
-            background = Image.new('RGB', img.size, (255, 255, 255))
-            background.paste(img, mask=img)
 
             # Манипуляция изменения размеров картинки
 
             if width > MAX_WIDTH:
                 new_w = MAX_WIDTH
                 new_h = int(height * (new_w / width))
-                background = background.resize((new_w, new_h), Image.LANCZOS)
+                img = img.resize((new_w, new_h), Image.LANCZOS)
 
+                background = Image.new('RGB', img.size, (255, 255, 255))
+                background.paste(img, mask=img)
+
+            background = Image.new('RGB', img.size, (255, 255, 255))
+            background.paste(img, mask=img)
 
             if background.mode != "RGB":
                 background = background.convert('RGB')
@@ -50,7 +54,7 @@ def processed_image(path, out_jpegs_dir, out_webps_dir):
 
             # Сохранение измененного файла
 
-            if width < 65535 and height < 65535:
+            if width < 65535 or height < 65535:
                 try:
                     background.save(out_jpeg, format='JPEG')
                     print(f"Сохранение JPEG -> {out_jpeg}")
@@ -59,7 +63,6 @@ def processed_image(path, out_jpegs_dir, out_webps_dir):
             else:
                 print('Слишком большой размер файла для jpeg')
                 return
-
 
             if width < 16383 or height < 16383:
                 try:
@@ -72,7 +75,7 @@ def processed_image(path, out_jpegs_dir, out_webps_dir):
 
                 return
 
-    # Различные ошибки
+        # Различные ошибки
 
         else:
             print('Неправильное расширение файла')
@@ -83,86 +86,48 @@ def processed_image(path, out_jpegs_dir, out_webps_dir):
     except Exception as e:
         print(f'Ошибка обработки {path}: {e}')
 
-# Функция для получения пути для файла
-
-def main_file():
-
-    # Открытие проводника для записи пути
-
-    script_dir = os.path.dirname(os.path.abspath(__file__))
-    images_dir = filedialog.askopenfilename()
-
-    # Ошибка на отсутствие файла
-
-    if images_dir == '':
-        print('Файл отсутствует')
-        return
-
-    # Создание путей для сохранения файла
-
-    out_dir = os.path.join(script_dir, 'out')
-    out_jpegs = os.path.join(out_dir, 'jpegs')
-    out_webps = os.path.join(out_dir, 'webps')
-
-    # Создание катологов
-
-    os.makedirs(out_jpegs, exist_ok=True)
-    os.makedirs(out_webps, exist_ok=True)
-
-    # Вызов функии для обработки изображения
-
-    processed_image(images_dir, out_jpegs, out_webps)
-
-# Функция для полученияя путей файлов в папке
-def main_papka():
-
-    # Открытие проводника для записи пути
-
-    script_dir = os.path.dirname(os.path.abspath(__file__))
-    images_dir = filedialog.askdirectory()
-
-    # Ошибка на отсутствие папки
-
-    if images_dir == '':
-        print('Файл отсутствует')
-        return
-
-    # Создание списка путей файлов
-
-    files = [os.path.join(images_dir, f) for f in os.listdir(images_dir)]
-
-    # Ошибка на пустую папки
-
-    if not files:
-        print('Папка с файлами пустая')
-        return
-
-    # Создание путей для сохранения файла
-
-    out_dir = os.path.join(script_dir, 'out')
-    out_jpegs = os.path.join(out_dir, 'jpegs')
-    out_webps = os.path.join(out_dir, 'webps')
-
-    # Создание катологов
-
-    os.makedirs(out_jpegs, exist_ok=True)
-    os.makedirs(out_webps, exist_ok=True)
-
-    # Вызов функии для обработки изображения
-
-    for file in files:
-        processed_image(file, out_jpegs, out_webps)
 
 # Основной процесс
 
 if __name__ == '__main__':
 
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+
+    # Создание путей для сохранения файла
+
+    out_dir = os.path.join(script_dir, 'out')
+    out_jpegs = os.path.join(out_dir, 'jpegs')
+    out_webps = os.path.join(out_dir, 'webps')
+
+    # Создание катологов
+
+    os.makedirs(out_jpegs, exist_ok=True)
+    os.makedirs(out_webps, exist_ok=True)
+
     parser = argparse.ArgumentParser()
     parser.add_argument('-f', type=str, required=True,
-                        help="Выберите формат работы: f - с файлом, p - с папкой")
+                        help="Выберите формат работы: file - с файлом, papka - с папкой")
+    parser.add_argument('path')
     args = parser.parse_args()
     f = args.f
-    if f == 'f':
-        main_file()
-    elif f == 'p':
-        main_papka()
+    if f == 'file':
+        images_dir = args.path
+
+        if images_dir == '':
+            print('Файл отсутствует')
+
+        processed_image(images_dir, out_jpegs, out_webps)
+
+    elif f == 'papka':
+        images_dir = args.path
+
+        if images_dir == '':
+            print('Файл отсутствует')
+
+        files = [os.path.join(images_dir, f) for f in os.listdir(images_dir)]
+
+        if not files:
+            print('Папка с файлами пустая')
+
+        for file in files:
+            processed_image(file, out_jpegs, out_webps)
